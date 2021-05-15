0a78844a7acb8a8084bc4b4e63665a517f3121df
Alex-Lu-hub <2326521374@qq.com>
2021-05-15 22:18:02 +0800

    add multi-files

---
+++ 07.Managing Growing Projects with Packages, Crates, and Modules/01.restaurant/src/front_of_house.rs
@@ -0,0 +1,15 @@
+// pub mod hosting {
+//     pub fn add_to_waitlist() {}
+
+//     fn seat_at_table() {}
+// }
+
+pub mod hosting;
+
+mod serving {
+    fn take_order() {}
+
+    fn server_order() {}
+
+    fn take_payment() {}
+}
--- 07.Managing Growing Projects with Packages, Crates, and Modules/01.restaurant/src/lib.rs
+++ 07.Managing Growing Projects with Packages, Crates, and Modules/01.restaurant/src/lib.rs
@@ -1,18 +1,22 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
 
-        fn seat_at_table() {}
-    }
+//         fn seat_at_table() {}
+//     }
 
-    mod serving {
-        fn take_order() {}
+//     mod serving {
+//         fn take_order() {}
 
-        fn server_order() {}
+//         fn server_order() {}
 
-        fn take_payment() {}
-    }
-}
+//         fn take_payment() {}
+//     }
+// }
+
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
 
 mod back_of_house {
     fn fix_incorrect_order() {
@@ -46,10 +50,13 @@ fn serve_order() {}
 
 pub fn eat_at_restaurant() {
     // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+    // crate::front_of_house::hosting::add_to_waitlist();
 
     // Relative path
-    front_of_house::hosting::add_to_waitlist();
+    // front_of_house::hosting::add_to_waitlist();
+
+    // Have use
+    hosting::add_to_waitlist();
 
     // Order a breakfast in the summer with Rye toast
     let mut meal = back_of_house::Breakfast::summer("Rye");
